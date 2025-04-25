934b3cc3d8e4351097afa801cfe6dd2d5ade98d3
David Chen <davidc3287@gmail.com>
2025-04-24 20:49:28 -0700

    finish norm

--- services/pg_normalized_batch/schema-indexes.sql
+++ services/pg_normalized_batch/schema-indexes.sql
@@ -1,2 +1,5 @@
--- comment
 CREATE INDEX idx_tag_idtweets ON tweet_tags(tag, id_tweets);
+CREATE INDEX idx_idtweets_tag ON tweet_tags(id_tweets, tag);
+CREATE INDEX idx_lang_idtweets ON tweets(lang, id_tweets);
+CREATE INDEX idx_text_fts ON tweets USING GIN(to_tsvector('english', text));
+CREATE INDEX idx_idtweets_lang ON tweets(id_tweets, lang);
