ab152965f0a27e5bc8c3b14d7d3a35db38bb3d93
David Chen <davidc3287@gmail.com>
2025-04-25 11:48:25 -0700

    clean up indexes

--- services/pg_denormalized/schema-indexes.sql
+++ services/pg_denormalized/schema-indexes.sql
@@ -1,49 +1,15 @@
-CREATE INDEX idx_all_hashtags_array
-ON public.tweets_jsonb
-USING gin (
-  (
-    COALESCE((data -> 'entities'::text) -> 'hashtags'::text, '[]'::jsonb)
-    ||
-    COALESCE(((data -> 'extended_tweet'::text) -> 'entities'::text) -> 'hashtags'::text, '[]'::jsonb)
-  )
-);
+CREATE INDEX idx_hashtags_coronavirus ON public.tweets_jsonb 
+USING gin ((COALESCE((data -> 'entities'::text) -> 'hashtags'::text, '[]'::jsonb) || 
+            COALESCE(((data -> 'extended_tweet'::text) -> 'entities'::text) -> 'hashtags'::text, '[]'::jsonb)))
+WHERE ((COALESCE((data -> 'entities'::text) -> 'hashtags'::text, '[]'::jsonb) || 
+        COALESCE(((data -> 'extended_tweet'::text) -> 'entities'::text) -> 'hashtags'::text, '[]'::jsonb))) @> '[{"text":"coronavirus"}]';
 
-CREATE INDEX idx_data_hashtags_text
-ON public.tweets_jsonb
-USING gin (
-  (
-    COALESCE((data -> 'entities'::text) -> 'hashtags'::text, '[]'::jsonb)
-    ||
-    COALESCE(((data -> 'extended_tweet'::text) -> 'entities'::text) -> 'hashtags'::text, '[]'::jsonb)
-  )
-);
+CREATE INDEX idx_corona_english_tweets ON public.tweets_jsonb
+USING gin (to_tsvector('english'::regconfig,
+           COALESCE((data -> 'extended_tweet'::text) ->> 'full_text'::text, data ->> 'text'::text)))
+WHERE (data ->> 'lang'::text) = 'en';
 
-CREATE INDEX idx_data_id
-ON public.tweets_jsonb
-USING btree ((data ->> 'id'::text));
-
-CREATE INDEX idx_data_lang
-ON public.tweets_jsonb
-USing btree ((data ->> 'lang'::text));
-
-CREATE INDEX idx_data_text_fts
-ON public.tweets_jsonb
-USING gin (
-  to_tsvector(
-    'english'::regconfig,
-    COALESCE(
-      (data -> 'extended_tweet'::text) ->> 'full_text'::text,
-      data ->> 'text'::text
-    )
-  )
-);
-
-CREATE INDEX idx_tweets_jsonb_all_hashtags
-ON public.tweets_jsonb
-USING gin (
-  (
-    COALESCE((data -> 'entities'::text) -> 'hashtags'::text, '[]'::jsonb)
-    ||
-    COALESCE(((data -> 'extended_tweet'::text) -> 'entities'::text) -> 'hashtags'::text, '[]'::jsonb)
-  )
-);
+CREATE INDEX idx_lang_coronavirus_hashtag ON public.tweets_jsonb
+USING btree ((data ->> 'lang'::text))
+WHERE ((COALESCE((data -> 'entities'::text) -> 'hashtags'::text, '[]'::jsonb) ||
+        COALESCE(((data -> 'extended_tweet'::text) -> 'entities'::text) -> 'hashtags'::text, '[]'::jsonb))) @> '[{"text":"coronavirus"}]';
